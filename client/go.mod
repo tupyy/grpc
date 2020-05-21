690c537a88d0c1e813336632758b111b0597993c
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2020-05-21 12:08:23 +0200

    add client

---
+++ client/go.mod
@@ -0,0 +1,9 @@
+module github.com/tupyy/grpc/client
+
+go 1.13
+
+require (
+	github.com/tupyy/grpc/helloworld v0.0.0-20200521095948-308003204902 // indirect
+	google.golang.org/grpc v1.30.0-dev.1
+	google.golang.org/grpc/examples v0.0.0-20200521004103-8bd46590eb55
+)
--- server/main.go
+++ server/main.go
@@ -14,14 +14,14 @@ type helloServer struct {
 	pb.GreeterServer
 }
 
-func (s *helloServer) SayHello(ctx context.Context, person *pb.Person) *pb.PersonResponse {
+func (s *helloServer) SayHello(ctx context.Context, person *pb.Person) (*pb.PersonResponse, error) {
 	fmt.Printf("Say hello: %+v\n", person)
-	return &pb.PersonResponse{"hello to you"}
+	return &pb.PersonResponse{Message: "hello to you"}, nil
 }
 
 func main() {
-	lis, _ := net.Listen("tcp", "localhost:7000")
+	lis, _ := net.Listen("tcp", "localhost:50000")
 	grpcServer := grpc.NewServer()
-	pb.RegisterGreeterServer(grpcServer)
+	pb.RegisterGreeterServer(grpcServer, &helloServer{})
 	grpcServer.Serve(lis)
 }
